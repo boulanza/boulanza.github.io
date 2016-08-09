668e415b11e32fdaff51359e1b17f1d1ff7d20a0
Zach Boulanger <boulanza@umich.edu>
2016-08-08 20:19:28 -0400

    skills section

--- dev.js
+++ dev.js
@@ -1,3 +1,15 @@
+// repeat function from: 
+// http://stackoverflow.com/questions/202605/repeat-string-javascript
+String.prototype.repeat = function(count) {
+	if (count < 1) return '';
+	var result = '', pattern = this.valueOf();
+	while (count > 1) {
+		if (count & 1) result += pattern;
+		count >>= 1, pattern += pattern;
+	}
+	return result + pattern;
+}
+
 $(document).ready(function() {
 	$('#expNav ul li').click(function() {
 		// remove current selection
@@ -8,5 +20,11 @@ $(document).ready(function() {
 		var num = $(this).attr('data-link');
 		$(this).addClass('selectedLink');
 		$('#exp' + num).addClass('selectedContent');
-	})
-})
+	});
+	$('.skill').each(function() {
+		var level = $(this).attr('data-level');
+		var colorBox = '<span class="colorBox"></span>';
+		var grayBox = '<span class="grayBox"></span>';
+		$(this).html(colorBox.repeat(level) + grayBox.repeat(5-level));
+	});
+});
