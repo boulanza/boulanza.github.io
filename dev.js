0b914ee68a195650e0c419a8c292a34408e88c4a
Zach Boulanger <boulanza@umich.edu>
2016-08-23 13:50:17 -0400

    redesigned website with bootstrap

--- dev.js
+++ dev.js
@@ -1,30 +1,12 @@
-// repeat function from: 
-// http://stackoverflow.com/questions/202605/repeat-string-javascript
-String.prototype.repeat = function(count) {
-	if (count < 1) return '';
-	var result = '', pattern = this.valueOf();
-	while (count > 1) {
-		if (count & 1) result += pattern;
-		count >>= 1, pattern += pattern;
-	}
-	return result + pattern;
-}
-
 $(document).ready(function() {
 	$('#expNav ul li').click(function() {
 		// remove current selection
-		$('#expNav ul li').removeClass('selectedLink');
+		$('#expNav ul li').removeClass('active');
 		$('.expContent').removeClass('selectedContent')
 
 		// add new selection
 		var num = $(this).attr('data-link');
-		$(this).addClass('selectedLink');
+		$(this).addClass('active');
 		$('#exp' + num).addClass('selectedContent');
-	});
-	$('.skill').each(function() {
-		var level = $(this).attr('data-level');
-		var colorBox = '<span class="colorBox"></span>';
-		var grayBox = '<span class="grayBox"></span>';
-		$(this).html(colorBox.repeat(level) + grayBox.repeat(5-level));
 	});
 });
